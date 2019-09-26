6c92ae727ff008b3db932649e9647a15b0897e4d
hchiam <hchiam@users.noreply.github.com>
2019-09-26 14:00:00 -0400

    hide hint and make learning link go directly

--- brain.js
+++ brain.js
@@ -6,7 +6,12 @@ function showHint() {
   document.getElementById('hint').style.visibility = 'visible';
 }
 
+function hideHint() {
+  document.getElementById('hint').style.visibility = 'hidden';
+}
+
 function surprise() {
   document.getElementById('learning-link').style.visibility = 'visible';
   alert('Hi!');
+  hideHint();
 }
