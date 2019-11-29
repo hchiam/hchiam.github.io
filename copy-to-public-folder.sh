624a1fd1e6aa517a4580d7771dd191554ed62ce4
hchiam <hchiam@users.noreply.github.com>
2019-11-28 22:29:07 -0500

    more cleanup

--- copy-to-public-folder.sh
+++ copy-to-public-folder.sh
@@ -24,5 +24,3 @@ cp sound.js public/sound.js
 cp style.css public/style.css
 cp offline-page.html public/offline-page.html
 cp service-worker.js public/service-worker.js
-cp jquery.min.js public/jquery.min.js
-cp jquery-ui.min.js public/jquery-ui.min.js
