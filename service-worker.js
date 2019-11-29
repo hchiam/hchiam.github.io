9b2ddd3a3e4dad85f2fa5af1e060265119940b6a
hchiam <hchiam@users.noreply.github.com>
2019-11-28 22:23:13 -0500

    simplify

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,9 @@
-var CACHE_NAME = 'version_03';
+var CACHE_NAME = 'version_04';
 
 var URLS = [
   'htc.png',
   'index.html',
   'offline-page.html',
-  'script.js',
-  'style.css',
-  'jquery.min.js',
-  'jquery-ui.min.js',
-  '/public/',
 ];
 
 self.addEventListener('install', function (e) {
@@ -27,8 +22,7 @@ self.addEventListener('fetch', function (e) {
         return caches.open(CACHE_NAME)
           .then(function (cache) {
             console.log('Service worker working even though you are offline.')
-            // return cache.match('offline-page.html');
-            return cache.matchAll('/public/');
+            return cache.match('offline-page.html');
           });
       })
     );
