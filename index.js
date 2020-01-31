8490a19a2d9ec16f303bd867606e45d7fd5b7ac1
hchiam <hchiam@gmail.com>
2020-01-31 09:35:08 -0500

    3000 -> 8000 [skip ci]

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 // be able to use files:
 app.use('/', express.static(__dirname + ''));
 
-// serve index.html at localhost:3000/
+// can serve index.html at localhost:8000/
 app.get("/", (request, response) => {
   response.sendFile(__dirname + '/index.html');
 });
 
 // make this app actually listen for requests:
-const listener = app.listen(process.env.PORT | 8000, () => { // | 3000 in case testing locally
+const listener = app.listen(process.env.PORT | 8000, () => { // | 8000 in case testing locally
   console.log('Your app is listening on port ' + listener.address().port);
 });
