5e2bf13ee4550a2e8d5ba7e7f2770e162002e8c5
hchiam <hchiam@gmail.com>
2020-02-05 23:52:43 -0500

    trigger update sw

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'hchiam.github.io-version_10'; // also can serve as cache name
+const SW_VERSION = 'hchiam.github.io-version_11'; // also can serve as cache name
 
 const appShellURLs = [
   '/jquery.min.js',
