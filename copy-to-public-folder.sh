994365fb99e2b60f8c54fc51c2f83b1e76e06e81
hchiam <hchiam@gmail.com>
2020-02-10 09:56:03 -0500

    try to fix

--- copy-to-public-folder.sh
+++ copy-to-public-folder.sh
@@ -12,6 +12,7 @@ fi
 rm public/*
 
 # copy specific files into the public folder:
+cp CNAME public/CNAME
 cp htc.png public/htc.png
 cp index.html public/index.html
 cp minified-code.js public/minified-code.js
