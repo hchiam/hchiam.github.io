0864d6e13b1337709f25b022ef03ea3d86fc675b
hchiam <hchiam@gmail.com>
2020-02-10 09:47:22 -0500

    http -> https redirect

--- brain.js
+++ brain.js
@@ -1,3 +1,8 @@
+// http -> https
+if (!String(window.location).startsWith('http://localhost') && window.location.protocol !== 'https:') {
+  window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+
 setTimeout(() => {
   slideIn();
   advertiseOfflineAbility();
