4dc8f8ee09c0209f084c6f09548c1367914c83ca
hchiam <hchiam@gmail.com>
2020-04-16 00:01:56 -0400

    Prettier formatting + hidden comment in page

--- brain.js
+++ brain.js
@@ -1,6 +1,10 @@
 // http -> https
-if (!String(window.location).startsWith('http://localhost') && window.location.protocol !== 'https:') {
-  window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+if (
+  !String(window.location).startsWith("http://localhost") &&
+  window.location.protocol !== "https:"
+) {
+  window.location.href =
+    "https:" + window.location.href.substring(window.location.protocol.length);
 }
 
 setTimeout(() => {
@@ -10,16 +14,19 @@ setTimeout(() => {
 
 setTimeout(() => {
   expandButtons();
-  document.getElementById('button-notification').innerText = 'Within scrolling view, there are six buttons in a group.';
+  document.getElementById("button-notification").innerText =
+    "Within scrolling view, there are six buttons in a group.";
 }, 1000);
 
 setTimeout(() => {
   if (!onDesktop()) {
-    document.getElementById('hint').innerText = 'Hint: scroll down';
-    document.getElementById('secret-button').innerText = 'Secret button!';
+    document.getElementById("hint").innerText = "Hint: scroll down";
+    document.getElementById("secret-button").innerText = "Secret button!";
   } else {
-    document.getElementById('hint').innerText = 'Hint: hit Tab, or Shift + Tab a few times.';
-    document.getElementById('secret-button').innerText = 'Hit Enter or Spacebar!';
+    document.getElementById("hint").innerText =
+      "Hint: hit Tab, or Shift + Tab a few times.";
+    document.getElementById("secret-button").innerText =
+      "Hit Enter or Spacebar!";
   }
   showHint();
 }, 2000);
@@ -27,58 +34,58 @@ setTimeout(() => {
 setUpGame();
 
 function slideIn() {
-  $('section').css({position: 'relative', top: '-100%'});
-  $('section').animate({top: 0});
+  $("section").css({ position: "relative", top: "-100%" });
+  $("section").animate({ top: 0 });
 }
 
 function advertiseOfflineAbility() {
   if (navigator.serviceWorker) {
-    $('#extra-info').text('(BTW: This page works offline!)');
+    $("#extra-info").text("(BTW: This page works offline!)");
   }
 }
 
 function expandButtons() {
-  var btns = Array.prototype.slice.call(document.querySelectorAll('button'));
-  for (var i=0; i<btns.length; i++) {
-    btns[i].classList.add('view-resize-animation');
+  var btns = Array.prototype.slice.call(document.querySelectorAll("button"));
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].classList.add("view-resize-animation");
   }
 }
 
 function onDesktop() {
-  return (window.screen.availWidth > 640);
+  return window.screen.availWidth > 640;
 }
 
 function showHint() {
-  document.getElementById('hint').style.visibility = 'visible';
+  document.getElementById("hint").style.visibility = "visible";
 }
 
 function hideHint() {
-  document.getElementById('hint').style.visibility = 'hidden';
+  document.getElementById("hint").style.visibility = "hidden";
 }
 
 function hideSecretButton() {
-  document.getElementById('secret-button').style.visibility = 'hidden';
+  document.getElementById("secret-button").style.visibility = "hidden";
 }
 
 function focusOnLearningLink() {
   // a skip link. https://classroom.udacity.com/courses/ud891/lessons/7962031279/concepts/79590516900923
-  document.querySelector('#learning-link button').focus();
+  document.querySelector("#learning-link button").focus();
 }
 
 function surprise() {
-  document.getElementById('learning-link').style.visibility = 'visible';
+  document.getElementById("learning-link").style.visibility = "visible";
   hideSecretButton();
   hideHint();
   setTimeout(() => {
     showHint();
     focusOnLearningLink();
     // do this last in case jQuery is unavailable:
-    $('#hint').text('Hint: drag the icon to move things around.');
+    $("#hint").text("Hint: drag the icon to move things around.");
   }, 100);
 }
 
 function getRandomNumber(start, stop) {
-  return Math.floor((Math.random() * stop) + start);
+  return Math.floor(Math.random() * stop + start);
 }
 
 // silly game
@@ -86,30 +93,30 @@ let gameOn = false;
 let turnedGameOnOnce = false;
 let continueGame = false;
 function setUpGame() {
-  $(document).ready(function() {
+  $(document).ready(function () {
     // game controls on:
-    document.getElementById('draggable-handle').onmouseenter = function() {
+    document.getElementById("draggable-handle").onmouseenter = function () {
       if (turnedGameOnOnce || onDesktop()) {
-        $('#draggable').draggable();
+        $("#draggable").draggable();
         hideHint();
         if (turnedGameOnOnce) {
           showGameButtons();
         }
       }
-    }
+    };
     // game controls off:
-    document.getElementById('draggable-handle').onmouseleave = function() {
+    document.getElementById("draggable-handle").onmouseleave = function () {
       if (onDesktop() && !continueGame) {
-        $('#draggable').draggable('destroy');
+        $("#draggable").draggable("destroy");
         resetGameButtons();
         gameOn = false;
       }
-    }
+    };
   });
 }
 
 function showGameButtons() {
-  var hasSmallScreen = (document.documentElement.clientWidth < 640);
+  var hasSmallScreen = document.documentElement.clientWidth < 640;
   if (hasSmallScreen) return;
 
   setUpGameButtons();
@@ -118,123 +125,170 @@ function showGameButtons() {
 }
 
 function setUpGameButtons() {
-  $('button:contains("GitHub")').text(' A ')
-    .attr('onclick', "jQuery.event.trigger({ type : 'keydown', keyCode : 65 });");
-  $('button:contains("CodePen")').text(' S ')
-    .attr('onclick', "jQuery.event.trigger({ type : 'keydown', keyCode : 83 });");
-  $('button:contains("Glitch")').text(' D ')
-    .attr('onclick', "jQuery.event.trigger({ type : 'keydown', keyCode : 68 });");
-  $('button:contains("LinkedIn")').text(' F ')
-    .attr('onclick', "jQuery.event.trigger({ type : 'keydown', keyCode : 70 });");
-  $('button:contains("Blog")').text(' C ')
-    .attr('onclick', "jQuery.event.trigger({ type : 'keydown', keyCode : 67 });");
-  $('button:contains("Memrise")').text(' SPACE ')
-    .attr('onclick', "jQuery.event.trigger({ type : 'keydown', keyCode : 32 });");
+  $('button:contains("GitHub")')
+    .text(" A ")
+    .attr(
+      "onclick",
+      "jQuery.event.trigger({ type : 'keydown', keyCode : 65 });"
+    );
+  $('button:contains("CodePen")')
+    .text(" S ")
+    .attr(
+      "onclick",
+      "jQuery.event.trigger({ type : 'keydown', keyCode : 83 });"
+    );
+  $('button:contains("Glitch")')
+    .text(" D ")
+    .attr(
+      "onclick",
+      "jQuery.event.trigger({ type : 'keydown', keyCode : 68 });"
+    );
+  $('button:contains("LinkedIn")')
+    .text(" F ")
+    .attr(
+      "onclick",
+      "jQuery.event.trigger({ type : 'keydown', keyCode : 70 });"
+    );
+  $('button:contains("Blog")')
+    .text(" C ")
+    .attr(
+      "onclick",
+      "jQuery.event.trigger({ type : 'keydown', keyCode : 67 });"
+    );
+  $('button:contains("Memrise")')
+    .text(" SPACE ")
+    .attr(
+      "onclick",
+      "jQuery.event.trigger({ type : 'keydown', keyCode : 32 });"
+    );
 }
 
 function resetGameButtons() {
-  $('button:contains(" A ")').text('GitHub')
-    .attr('onclick', "window.open('https:\/\/github.com/hchiam', '_blank');");
-  $('button:contains(" S ")').text('CodePen')
-    .attr('onclick', "window.open('https:\/\/codepen.io/hchiam', '_blank');");
-  $('button:contains(" D ")').text('Glitch')
-    .attr('onclick', "window.open('https:\/\/glitch.com/@hchiam', '_blank');");
-  $('button:contains(" F ")').text('LinkedIn')
-    .attr('onclick', "window.open('https:\/\/ca.linkedin.com/in/howardchiam', '_blank');");
-  $('button:contains(" C ")').text('Blog')
-    .attr('onclick', "window.open('https:\/\/hchiam.blogspot.com', '_blank');");
-  $('button:contains(" SPACE ")').text('Memrise')
-    .attr('onclick', "window.open('https:\/\/www.memrise.com/user/hchiam/courses/learning', '_blank');");
+  $('button:contains(" A ")')
+    .text("GitHub")
+    .attr("onclick", "window.open('https://github.com/hchiam', '_blank');");
+  $('button:contains(" S ")')
+    .text("CodePen")
+    .attr("onclick", "window.open('https://codepen.io/hchiam', '_blank');");
+  $('button:contains(" D ")')
+    .text("Glitch")
+    .attr("onclick", "window.open('https://glitch.com/@hchiam', '_blank');");
+  $('button:contains(" F ")')
+    .text("LinkedIn")
+    .attr(
+      "onclick",
+      "window.open('https://ca.linkedin.com/in/howardchiam', '_blank');"
+    );
+  $('button:contains(" C ")')
+    .text("Blog")
+    .attr("onclick", "window.open('https://hchiam.blogspot.com', '_blank');");
+  $('button:contains(" SPACE ")')
+    .text("Memrise")
+    .attr(
+      "onclick",
+      "window.open('https://www.memrise.com/user/hchiam/courses/learning', '_blank');"
+    );
 }
 
-const mysterySpawnButton = '<button onclick="$(this).text(\'\').css({background:\'blue\',color:\'white\',width:0,height:0,padding:0});var t = this;setTimeout(function(){t.parentNode.removeChild(t)}, 500);">?</button>';
+const mysterySpawnButton =
+  "<button onclick=\"$(this).text('').css({background:'blue',color:'white',width:0,height:0,padding:0});var t = this;setTimeout(function(){t.parentNode.removeChild(t)}, 500);\">?</button>";
 
-$(document).keydown(function(e) {
+$(document).keydown(function (e) {
   if (gameOn) {
-    if (e.keyCode === 65) { // a
-      $('#game-container').append(mysterySpawnButton);
-    } else if (e.keyCode === 83) { // s
-      $('#game-container *:last-child').remove();
-    } else if (e.keyCode === 68) { // d
-      $('#game-container *').remove();
-    } else if (e.keyCode === 70) { // f
-      $('#game-container *').remove();
-      for (let i=0; i<30; i++) {
-        $('#game-container').append(mysterySpawnButton);
+    if (e.keyCode === 65) {
+      // a
+      $("#game-container").append(mysterySpawnButton);
+    } else if (e.keyCode === 83) {
+      // s
+      $("#game-container *:last-child").remove();
+    } else if (e.keyCode === 68) {
+      // d
+      $("#game-container *").remove();
+    } else if (e.keyCode === 70) {
+      // f
+      $("#game-container *").remove();
+      for (let i = 0; i < 30; i++) {
+        $("#game-container").append(mysterySpawnButton);
       }
-    } else if (e.keyCode === 67) { // c
+    } else if (e.keyCode === 67) {
+      // c
       continueGame = !continueGame;
       if (!continueGame) {
         // game controls off:
-        $('#draggable').draggable('destroy');
+        $("#draggable").draggable("destroy");
         resetGameButtons();
         gameOn = false;
       }
     }
   }
-  if (turnedGameOnOnce && e.keyCode === 32) { // space
+  if (turnedGameOnOnce && e.keyCode === 32) {
+    // space
     showHint();
-    $('#hint').text('Hint: refresh the page.');
-    childrenExodus('section');
-    childrenExodus('#draggable');
+    $("#hint").text("Hint: refresh the page.");
+    childrenExodus("section");
+    childrenExodus("#draggable");
     makeCommandKeysConspicuous();
     scatterSpace();
   }
 });
 
 function childrenExodus(parentSelector) {
-  let children = $(parentSelector + ' *').detach();
-  $('body').append(children);
+  let children = $(parentSelector + " *").detach();
+  $("body").append(children);
   $(parentSelector).remove();
 }
 
 function makeCommandKeysConspicuous() {
-  $('button:contains(" A ")').addClass('commandKey');
-  $('button:contains(" S ")').addClass('commandKey');
-  $('button:contains(" D ")').addClass('commandKey');
-  $('button:contains(" F ")').addClass('commandKey');
-  $('button:contains(" C ")').addClass('commandKey');
-  $('button:contains(" SPACE ")').addClass('commandKey');
+  $('button:contains(" A ")').addClass("commandKey");
+  $('button:contains(" S ")').addClass("commandKey");
+  $('button:contains(" D ")').addClass("commandKey");
+  $('button:contains(" F ")').addClass("commandKey");
+  $('button:contains(" C ")').addClass("commandKey");
+  $('button:contains(" SPACE ")').addClass("commandKey");
 }
 
 let scatteredSpace = false;
 function scatterSpace() {
-  $('button, img, #hint, h1, p').each(function() {
+  $("button, img, #hint, h1, p").each(function () {
     const myWidth = $(this).width();
     const myHeight = $(this).height();
-    const randomleft = getRandomNumber(0, window.innerWidth-myWidth);
-    const randomtop = getRandomNumber(0, window.innerHeight-myHeight);
+    const randomleft = getRandomNumber(0, window.innerWidth - myWidth);
+    const randomtop = getRandomNumber(0, window.innerHeight - myHeight);
     $(this)
       .css({
-        position: 'absolute',
+        position: "absolute",
       })
-      .animate({
-        left: randomleft + "px",
-        top: randomtop + "px",
-      }, 'fast', 'swing');
+      .animate(
+        {
+          left: randomleft + "px",
+          top: randomtop + "px",
+        },
+        "fast",
+        "swing"
+      );
   });
   scatteredSpace = true;
 }
 
 function imageFallback() {
-  $('img').remove();
-  $('#ascii-art').show();
+  $("img").remove();
+  $("#ascii-art").show();
 }
 
 function disableOldStyles() {
-  $('link[href="style.css"]').prop('disabled', true);
-  $('link[href="/style.css"]').prop('disabled', true);
-  $('link[href="minified-style.css"]').prop('disabled', true);
+  $('link[href="style.css"]').prop("disabled", true);
+  $('link[href="/style.css"]').prop("disabled", true);
+  $('link[href="minified-style.css"]').prop("disabled", true);
 }
 
 const tilt = UniversalTilt.init({
-  elements: document.querySelectorAll('.tilt'),
+  elements: document.querySelectorAll(".tilt"),
   settings: {
     // options...
   },
   callbacks: {
     // callbacks...
-  }
+  },
 });
 
 function goToGame() {
@@ -242,40 +296,58 @@ function goToGame() {
 }
 
 function goToSNPromptGenerator() {
-  $(function(){
-    $('body').load('https://cdn.jsdelivr.net/gh/hchiam/sn-prompt-generator@master/index.html', function() {
-      disableOldStyles();
-      $('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hchiam/sn-prompt-generator@master/style.css">');
-      $('head').append('<script src="https://cdn.jsdelivr.net/gh/hchiam/sn-prompt-generator@master/script.js"></script>');
-    });
+  $(function () {
+    $("body").load(
+      "https://cdn.jsdelivr.net/gh/hchiam/sn-prompt-generator@master/index.html",
+      function () {
+        disableOldStyles();
+        $("head").append(
+          '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hchiam/sn-prompt-generator@master/style.css">'
+        );
+        $("head").append(
+          '<script src="https://cdn.jsdelivr.net/gh/hchiam/sn-prompt-generator@master/script.js"></script>'
+        );
+      }
+    );
   });
 }
 
 function goToCRPrep() {
-  $(function(){
-    $('body').load('https://cdn.jsdelivr.net/gh/hchiam/cr-prep@master/index.html #button-bar, #checkbox-box, #hover-info', function() {
-      disableOldStyles();
-      $('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hchiam/cr-prep@master/style.css">');
-      $('head').append('<script src="https://cdn.jsdelivr.net/gh/hchiam/cr-prep@master/download-page-as-image.js"></script>');
-      $('head').append('<script src="https://cdn.jsdelivr.net/gh/hchiam/cr-prep@master/script.js"></script>');
-      $('head').append('<script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>');
-    });
+  $(function () {
+    $("body").load(
+      "https://cdn.jsdelivr.net/gh/hchiam/cr-prep@master/index.html #button-bar, #checkbox-box, #hover-info",
+      function () {
+        disableOldStyles();
+        $("head").append(
+          '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hchiam/cr-prep@master/style.css">'
+        );
+        $("head").append(
+          '<script src="https://cdn.jsdelivr.net/gh/hchiam/cr-prep@master/download-page-as-image.js"></script>'
+        );
+        $("head").append(
+          '<script src="https://cdn.jsdelivr.net/gh/hchiam/cr-prep@master/script.js"></script>'
+        );
+        $("head").append(
+          '<script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>'
+        );
+      }
+    );
   });
 }
 
 function skew() {
-  $('body').css({
-    'transform': 'skewY(-10deg)',
-    'transform-origin': '150% top',
-    'background': 'black',
+  $("body").css({
+    transform: "skewY(-10deg)",
+    "transform-origin": "150% top",
+    background: "black",
   });
-  setTimeout(function() {
-    $('body').css({
-      'transform': '',
-      'background': 'transparent',
-      'top': '0',
-    })
-  }, 1000);  
+  setTimeout(function () {
+    $("body").css({
+      transform: "",
+      background: "transparent",
+      top: "0",
+    });
+  }, 1000);
 }
 
 function home() {
@@ -285,7 +357,8 @@ function home() {
 const game = goToGame;
 const snpg = goToSNPromptGenerator;
 const cr = goToCRPrep;
-console.log(`%cIf you have an internet connection,
+console.log(
+  `%cIf you have an internet connection,
 you can enter these commands:%c
 
 %cgame()%c
@@ -299,10 +372,16 @@ you can enter these commands:%c
 %chome()%c
 
 `,
-'color: blue; background: lightgrey;','',
-'color: lime; background: black; padding: 5px 10px;','',
-'color: lime; background: black; padding: 5px 10px;','',
-'color: lime; background: black; padding: 5px 10px;','',
-'color: lime; background: black; padding: 5px 10px;','',
-'color: lime; background: black; padding: 5px 10px;','',
+  "color: blue; background: lightgrey;",
+  "",
+  "color: lime; background: black; padding: 5px 10px;",
+  "",
+  "color: lime; background: black; padding: 5px 10px;",
+  "",
+  "color: lime; background: black; padding: 5px 10px;",
+  "",
+  "color: lime; background: black; padding: 5px 10px;",
+  "",
+  "color: lime; background: black; padding: 5px 10px;",
+  ""
 );
--- cursor.js
+++ cursor.js
@@ -1,7 +1,7 @@
 // create a subtle shadow that follows the cursor:
-$(document).on('mousemove', function(e){
-  $('#cursor-shadow').css({
+$(document).on("mousemove", function (e) {
+  $("#cursor-shadow").css({
     left: e.pageX,
-    top: e.pageY
+    top: e.pageY,
   });
 });
