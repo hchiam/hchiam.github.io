45de2d47724ee16310cd94a015fc0c8755c49e49
hchiam <hchiam@gmail.com>
2020-06-09 22:43:42 -0400

    update csp with manifest

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "hchiam.github.io-version_16"; // also can serve as cache name
+const SW_VERSION = "hchiam.github.io-version_17"; // also can serve as cache name
 
 const appShellURLs = [
   "/jquery.min.js",
