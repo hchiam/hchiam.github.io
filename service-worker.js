fc4108445cb9bdcf18944f20c435249ae4745c01
hchiam <hchiam@gmail.com>
2020-06-11 22:08:53 -0400

    add flying focus ring (fix #17)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "hchiam.github.io-version_17"; // also can serve as cache name
+const SW_VERSION = "hchiam.github.io-version_18"; // also can serve as cache name
 
 const appShellURLs = [
   "/jquery.min.js",
