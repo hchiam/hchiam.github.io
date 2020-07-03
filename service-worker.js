d63310eb6841826fd00d968fe8ff78e639ea28f0
hchiam <hchiam@users.noreply.github.com>
2020-07-02 23:35:11 -0400

    update minified code and service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "hchiam.github.io-version_19"; // also can serve as cache name
+const SW_VERSION = "hchiam.github.io-version_20"; // also can serve as cache name
 
 const appShellURLs = [
   "/jquery.min.js",
