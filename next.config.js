d48ebbedafd6e0a9b3a2cbd8d493f89d5b991810
hchiam <hchiam@users.noreply.github.com>
2021-02-12 23:27:35 -0500

    try this [skip ci]

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require("next-pwa");
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = withPWA({
+  assetPrefix: isProd ? "/gh-pages/" : "",
   pwa: {
     dest: "public",
   },
