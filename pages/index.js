7e177fc7340dd13a4f81634ad31041011dbc7fdf
hchiam <hchiam@users.noreply.github.com>
2021-08-02 14:36:10 -0400

    show demos [skip ci]

--- pages/index.js
+++ pages/index.js
@@ -468,7 +468,7 @@ export default function Home() {
             </div>
           )}
         </section>
-        <section id="demos" className="transparent-background d-none">
+        <section id="demos" className="transparent-background">
           <section className="demo transparent-background">
             <button
               id="waymo"
