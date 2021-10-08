a0488dbdb349b6c3ee3041553ed2ba0d499599c0
hchiam <hchiam@users.noreply.github.com>
2021-10-08 17:23:15 -0400

    fix ES3/ES5 type error [skip ci]

--- helpers/advertiseOfflineAbility.tsx
+++ helpers/advertiseOfflineAbility.tsx
@@ -1,13 +1,14 @@
 export default function advertiseOfflineAbility(element) {
   if (navigator.serviceWorker) {
     element.innerText = "(BTW: This page works offline!)";
-    document.addEventListener("scroll", hideMessageWhenScrolled);
+    document.addEventListener("scroll", hideMessageWhenScrolled.bind(element));
+  }
+}
 
-    function hideMessageWhenScrolled() {
-      if (window.scrollY > 0) {
-        element.innerText = "";
-        document.removeEventListener("scroll", hideMessageWhenScrolled);
-      }
-    }
+function hideMessageWhenScrolled() {
+  const element = this;
+  if (window.scrollY > 0) {
+    element.innerText = "";
+    document.removeEventListener("scroll", hideMessageWhenScrolled);
   }
 }
