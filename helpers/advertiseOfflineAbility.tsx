da7c6a0228a1add87326624376a03536e5fa9efb
hchiam <hchiam@users.noreply.github.com>
2021-10-08 17:21:05 -0400

    remove offline message when scroll [skip ci]

--- helpers/advertiseOfflineAbility.tsx
+++ helpers/advertiseOfflineAbility.tsx
@@ -1,5 +1,13 @@
 export default function advertiseOfflineAbility(element) {
   if (navigator.serviceWorker) {
     element.innerText = "(BTW: This page works offline!)";
+    document.addEventListener("scroll", hideMessageWhenScrolled);
+
+    function hideMessageWhenScrolled() {
+      if (window.scrollY > 0) {
+        element.innerText = "";
+        document.removeEventListener("scroll", hideMessageWhenScrolled);
+      }
+    }
   }
 }
