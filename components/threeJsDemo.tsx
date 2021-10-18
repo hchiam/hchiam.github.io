baf824ef8a975ab93f5a7f791020d2933c8b1f06
hchiam <hchiam@users.noreply.github.com>
2021-10-17 22:50:07 -0400

    try window["THREE"] to satisfy deploy script [skip ci]

--- components/threeJsDemo.tsx
+++ components/threeJsDemo.tsx
@@ -7,14 +7,14 @@ export function threeJsDemo(containerSelector) {
   const aspectRatio =
     document.documentElement.clientWidth /
     document.documentElement.clientHeight;
-  const camera = new window.THREE.PerspectiveCamera(
+  const camera = new window["THREE"].PerspectiveCamera(
     100,
     aspectRatio,
     0.1,
     10000
   );
-  const scene = new window.THREE.Scene();
-  const renderer = new window.THREE.WebGLRenderer({
+  const scene = new window["THREE"].Scene();
+  const renderer = new window["THREE"].WebGLRenderer({
     antialias: true,
     alpha: true,
   });
@@ -27,17 +27,17 @@ export function threeJsDemo(containerSelector) {
   setUpCanvas();
 
   function setUpMesh() {
-    const myMaterial = new window.THREE.MeshPhongMaterial({
-      color: new window.THREE.Color("green"),
-      emissive: new window.THREE.Color("green"),
-      specular: new window.THREE.Color("#636363"),
+    const myMaterial = new window["THREE"].MeshPhongMaterial({
+      color: new window["THREE"].Color("green"),
+      emissive: new window["THREE"].Color("green"),
+      specular: new window["THREE"].Color("#636363"),
       shininess: 10,
-      shading: window.THREE.FlatShading,
+      shading: window["THREE"].FlatShading,
       opacity: 0.75,
     });
 
-    mesh = new window.THREE.Mesh(
-      new window.THREE.IcosahedronGeometry(50, 1),
+    mesh = new window["THREE"].Mesh(
+      new window["THREE"].IcosahedronGeometry(50, 1),
       myMaterial
     );
     mesh.rotation.z = 0.5;
@@ -63,13 +63,13 @@ export function threeJsDemo(containerSelector) {
   function setUpScene() {
     scene.add(camera);
 
-    const light1 = new window.THREE.PointLight(0xffffff, 1);
+    const light1 = new window["THREE"].PointLight(0xffffff, 1);
     light1.position.z = 100;
     light1.position.y = 100;
     light1.position.x = 100;
     scene.add(light1);
 
-    const light2 = new window.THREE.PointLight(0xffffff, 0.8);
+    const light2 = new window["THREE"].PointLight(0xffffff, 0.8);
     light2.position.z = 200;
     light2.position.y = 50;
     light2.position.x = -100;
