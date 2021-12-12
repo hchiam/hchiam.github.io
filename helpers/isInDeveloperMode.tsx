9a34079c7cd0d3d7426c9135fe08601dcbfc64c5
hchiam <hchiam@users.noreply.github.com>
2021-12-12 17:22:12 -0500

    fix dev mode console error message [skip ci]

--- helpers/isInDeveloperMode.tsx
+++ helpers/isInDeveloperMode.tsx
@@ -1,5 +1,6 @@
 export default function isInDeveloperMode(): boolean {
   return (
-    location?.hostname === "localhost" || location?.hostname === "127.0.0.1"
+    typeof location !== "undefined" &&
+    (location?.hostname === "localhost" || location?.hostname === "127.0.0.1")
   );
 }
