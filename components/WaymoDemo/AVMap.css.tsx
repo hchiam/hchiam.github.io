eac1447a24a00233951ab84e9b981aa6bd07fd3b
hchiam <hchiam@users.noreply.github.com>
2022-02-17 18:05:33 -0500

    move --clip-path out of AVMap.css.tsx into globals.css :root [skip-ci]

--- components/WaymoDemo/AVMap.css.tsx
+++ components/WaymoDemo/AVMap.css.tsx
@@ -3,23 +3,6 @@ import css from "styled-jsx/css";
 // TODO: consider using GSAP instead for better perf
 
 export default css`
-  --clip-path: polygon(
-    51px 2px,
-    20.51% 18.63%,
-    8.47% 15.46%,
-    12.36% 31.72%,
-    10.16% 38.24%,
-    20.28% 50.53%,
-    15.42% 56.42%,
-    15.46% 63.34%,
-    11.65% 71.36%,
-    16.42% 76.14%,
-    9.63% 82.81%,
-    57.2% 99.55%,
-    97.9% 100.4%,
-    99.22% 1.45%
-  );
-
   .av-map {
     display: inline-block;
     position: relative;
