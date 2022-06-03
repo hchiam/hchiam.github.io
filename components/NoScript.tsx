770ce89819010e392bef92f9c88408958e316da5
hchiam <hchiam@users.noreply.github.com>
2022-06-03 17:59:05 -0600

    trim

---
+++ components/NoScript.tsx
@@ -0,0 +1,19 @@
+export default function NoScript() {
+  return (
+    <noscript>
+      <div id="noscript">
+        <p>Please enable JavaScript to learn more about Howard :)</p>
+        <p>
+          Or visit
+          <a
+            id="link-to-github-profile"
+            href="https://github.com/hchiam"
+            target="_blank"
+          >
+            Howard's GitHub profile
+          </a>
+        </p>
+      </div>
+    </noscript>
+  );
+}
