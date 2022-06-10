39865ef7a451eba707b96bf9d2ef2b3cf5e7946f
hchiam <hchiam@users.noreply.github.com>
2022-06-10 12:32:11 -0600

    deploy:gh-pages and deploy have been working so far before this commit

--- helpers/addUrlQueryWithoutRefreshingPage.ts
+++
@@ -1,6 +0,0 @@
-export default function addUrlQueryWithoutRefreshingPage(query: string): void {
-  if (window.history) {
-    const urlWithQuery: string = window.location.origin + "/?" + query;
-    window.history.pushState({ path: urlWithQuery }, "", urlWithQuery);
-  }
-}
--- helpers/getMockData.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default async function getMockData(pathName: string = "") {
-  const data = await axios.get(
-    "https://mock-hchiam-site-data.hchiam.workers.dev/" + pathName
-  );
-  return data.data;
-}
