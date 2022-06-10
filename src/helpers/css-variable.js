259b479b137aefc70b49bb7725dc9a7ac29fc838
hchiam <hchiam@users.noreply.github.com>
2022-06-10 16:56:48 -0600

    Deploy to gh-pages/docs

--- src/helpers/css-variable.js
+++ src/helpers/css-variable.js
@@ -14,15 +14,12 @@ console.log(getCSSVariable(cssVariableName));
 setCSSVariable(cssVariableName, "lime");
 */
 
-export function getCSSVariable(name: string): string {
+export function getCSSVariable(name) {
   return getComputedStyle(document.querySelector(":root"))
     .getPropertyValue(name)
     .trim();
 }
 
-export function setCSSVariable(name: string, value: string): void {
-  (document.querySelector(":root") as HTMLElement).style.setProperty(
-    name,
-    value
-  );
+export function setCSSVariable(name, value) {
+  document.querySelector(":root").style.setProperty(name, value);
 }
