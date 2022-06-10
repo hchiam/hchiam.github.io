1d84ddf5c6241c4473b40dd4aea6c204c2406835
hchiam <hchiam@users.noreply.github.com>
2022-06-10 12:00:52 -0600

    see if this works now - next->astro in one commit

--- helpers/cursor.tsx
+++
@@ -1,8 +0,0 @@
-export default function setUpCursorShadow(): void {
-  // create a subtle shadow that follows the cursor:
-  document.addEventListener("mousemove", function (e) {
-    const cursorShadow: HTMLElement = document.getElementById("cursor-shadow");
-    cursorShadow.style.left = e.pageX + "px";
-    cursorShadow.style.top = e.pageY + "px";
-  });
-}
