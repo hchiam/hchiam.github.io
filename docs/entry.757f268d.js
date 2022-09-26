7d3ff6f8c3684568717afc3ff93b77c56e3d3925
hchiam <hchiam@users.noreply.github.com>
2022-09-26 15:58:28 -0600

    Deploy to gh-pages/docs

---
+++ docs/entry.757f268d.js
@@ -0,0 +1,5 @@
+import{c as f,a as g,b as u,r as k,d as t}from"./chunks/chunk.c066d72e.js";/* empty css                     */const L=f("/@fs/Users/howard/Desktop/hchiam.github.io/src/components/ImageLink.astro",{modules:[],hydratedComponents:[],clientOnlyComponents:[],hydrationDirectives:new Set([]),hoisted:[]}),y=g("/@fs/Users/howard/Desktop/hchiam.github.io/src/components/ImageLink.astro","https://astro.build","file:///Users/howard/Desktop/hchiam.github.io/"),b=u(async(s,o,a)=>{const e=s.createAstro(y,o,a);e.self=b;const{href:r,alt:n,src:i,width:c,height:h,target:d="_blank",aStyle:l,imgStyle:m}=e.props,p=[];for(const $ of p)s.styles.add($);return k`<a${t(r,"href")}${t(d,"target")}${t(l,"style")} rel="noopener noreferrer" class="astro-XNNZEBEB">
+  <img${t(n,"alt")}${t(i,"src")}${t(c,"width")}${t(h,"height")}${t(m,"style")} class="astro-XNNZEBEB">
+</a>
+
+`}),S="/Users/howard/Desktop/hchiam.github.io/src/components/ImageLink.astro",A=void 0;export{L as $$metadata,b as default,S as file,A as url};
