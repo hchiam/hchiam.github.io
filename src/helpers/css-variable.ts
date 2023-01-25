94b4b666556599100ab7b44ad612524d55a82b28
hchiam <hchiam@users.noreply.github.com>
2023-01-24 18:26:42 -0700

    convert js orbit animations to 3D css orbit animations

--- src/helpers/css-variable.ts
+++ src/helpers/css-variable.ts
@@ -14,15 +14,21 @@ console.log(getCSSVariable(cssVariableName));
 setCSSVariable(cssVariableName, "lime");
 */
 
-export function getCSSVariable(name: string): string {
-  return getComputedStyle(document.querySelector(":root"))
+export function getCSSVariable(
+  name: string,
+  element?: HTMLElement | Element
+): string {
+  return getComputedStyle(element || document.querySelector(":root"))
     .getPropertyValue(name)
     .trim();
 }
 
-export function setCSSVariable(name: string, value: string): void {
-  (document.querySelector(":root") as HTMLElement).style.setProperty(
-    name,
-    value
-  );
+export function setCSSVariable(
+  name: string,
+  value: string,
+  element?: HTMLElement | Element
+): void {
+  (
+    element || (document.querySelector(":root") as HTMLElement)
+  ).style.setProperty(name, value);
 }
