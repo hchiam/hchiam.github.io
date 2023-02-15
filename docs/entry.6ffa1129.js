ef8c38a5e55bf39604d44829fba79a99cdfac2ea
hchiam <hchiam@users.noreply.github.com>
2023-02-14 22:11:38 -0700

    Deploy to gh-pages/docs

---
+++ docs/entry.6ffa1129.js
@@ -0,0 +1,18 @@
+import{c as g,a as b,b as S,r as a,d as t}from"./chunks/chunk.c066d72e.js";/* empty css                     */const L=g("/@fs/Users/howard/Desktop/hchiam.github.io/src/components/ImageLink.astro",{modules:[],hydratedComponents:[],clientOnlyComponents:[],hydrationDirectives:new Set([]),hoisted:[{type:"inline",value:`
+  function handleClick(href) {
+    document.querySelector(\`a[href="\${href}"]\`).click();
+  }
+  [...document.querySelectorAll("button[data-href]")].forEach((b) =>
+    b.addEventListener("click", function (e) {
+      const href = e.target.getAttribute("data-href");
+      handleClick(href);
+    })
+  );
+`}]}),k=b("/@fs/Users/howard/Desktop/hchiam.github.io/src/components/ImageLink.astro","https://astro.build","file:///Users/howard/Desktop/hchiam.github.io/"),y=S(async(e,o,n)=>{const r=e.createAstro(k,o,n);r.self=y;const{href:s,alt:c,src:i,width:h,height:l,target:d="_blank",aStyle:f,imgStyle:u,extraButton:m=!0}=r.props,$=[];for(const p of $)e.styles.add(p);return a`<a${t(s,"href")}${t(d,"target")}${t(f,"style")} rel="noopener noreferrer" class="astro-XK7PSBAS">
+  <img${t(c,"alt")}${t(i,"src")}${t(h,"width")}${t(l,"height")}${t(u,"style")} class="astro-XK7PSBAS">
+</a>
+${m?a`<button${t(s,"data-href")} class="astro-XK7PSBAS">VIEW</button>`:""}
+
+
+
+`}),C="/Users/howard/Desktop/hchiam.github.io/src/components/ImageLink.astro",D=void 0;export{L as $$metadata,y as default,C as file,D as url};
---
+++ docs/entry.bbe82417.js
@@ -0,0 +1,5 @@
+import{c as p,a as g,b as l,r as $,d as t}from"./chunks/chunk.c066d72e.js";/* empty css                     */const I=p("/@fs/Users/howard/Desktop/hchiam.github.io/src/components/LogoImg.astro",{modules:[],hydratedComponents:[],clientOnlyComponents:[],hydrationDirectives:new Set([]),hoisted:[]}),f=g("/@fs/Users/howard/Desktop/hchiam.github.io/src/components/LogoImg.astro","https://astro.build","file:///Users/howard/Desktop/hchiam.github.io/"),u=l(async(s,e,a)=>{const o=s.createAstro(f,e,a);o.self=u;const{src:r,alt:i,width:n,height:c,tabindex:d}=o.props,h=[];for(const m of h)s.styles.add(m);return $`<div class="wrapper astro-OJNI4CQX">
+  <img${t(r,"src")}${t(i,"alt")} loading="eager"${t(n||"","width")}${t(c||"","height")}${t(d||"","tabindex")} class="astro-OJNI4CQX">
+</div>
+
+`}),L="/Users/howard/Desktop/hchiam.github.io/src/components/LogoImg.astro",y=void 0;export{I as $$metadata,u as default,L as file,y as url};
