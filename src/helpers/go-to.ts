41e6561d47729235a2a7c95dd52299e34d9abece
hchiam <hchiam@users.noreply.github.com>
2023-06-25 19:28:08 -0600

    add back in some hidden features

---
+++ src/helpers/go-to.ts
@@ -0,0 +1,15 @@
+window.home = () => {
+  window.location = "/";
+};
+
+window.game = () => {
+  window.location = "/game";
+};
+
+window.notepad = () => {
+  window.location = "/notepad";
+};
+
+window.ux = () => {
+  window.location = "/ux";
+};
