671c171bef20962ffdba70329f3a7bfe2af0bf36
hchiam <hchiam@users.noreply.github.com>
2023-07-16 17:18:14 -0600

    replace @astrojs/node return Astro.redirect with window.location.href

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: "hybrid",
-  adapter: node({
-    mode: "middleware",
-  }),
 });
