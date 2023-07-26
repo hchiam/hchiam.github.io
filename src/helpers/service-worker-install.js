123d904650c93aeb3790576e5d1943d67ee319e8
hchiam <hchiam@users.noreply.github.com>
2023-07-25 22:18:37 -0600

    test offline.html with workbox service worker

---
+++ src/helpers/service-worker-install.js
@@ -0,0 +1,31 @@
+// Register the service worker
+if ("serviceWorker" in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener("load", async () => {
+    console.log("start");
+    await fetch("/offline.html");
+    console.log("continue");
+    // Try to register the service worker.
+    try {
+      // Capture the registration for later use, if needed
+      let registration;
+
+      const swPath = "/src/helpers/service-worker.js";
+
+      // import.meta.env.DEV is a special environment variable injected by Vite to let us know we're in development mode. Here, we can use the JS Module form of our service worker because we can control our browsers in dev.
+      if (import.meta.env.DEV) {
+        registration = await navigator.serviceWorker.register(swPath, {
+          type: "module",
+        });
+        console.log("Service worker registered! 😎", registration);
+      } else {
+        // In production, we use the normal service worker registration
+        registration = await navigator.serviceWorker.register(swPath);
+      }
+    } catch (err) {
+      if (import.meta.env.DEV) {
+        console.log("😥 Service worker registration failed: ", err);
+      }
+    }
+  });
+}
