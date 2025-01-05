05ca747970e58f26bf8818ebcaaaaa76e0c15878
hchiam <hchiam@users.noreply.github.com>
2025-01-05 14:26:09 -0700

    Deploy to gh-pages/docs

---
+++ docs/_astro/hoisted.B1vEbpbi.js
@@ -0,0 +1,1 @@
+window.home=()=>{window.location="/"};window.game=()=>{window.location="/game"};window.notepad=()=>{window.location="/notepad"};window.ux=()=>{window.location="/ux"};
---
+++ docs/_astro/hoisted.pMyOjHJB.js
@@ -0,0 +1,1 @@
+import"./hoisted.B1vEbpbi.js";
